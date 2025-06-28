002c0a22627ba3da1228e8f5aaac096175d18bdb
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-06-28 13:51:22 -0300

    correcciones

--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -1,35 +1,37 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
+import { environment } from '../../enviroments/enviroment';
 
 @Injectable({ providedIn: 'root' })
 export class UsuarioService {
+  private apiUrl = environment.apiUrl + '/usuarios';
+
   async registrarUsuario(nombre_usuario: string, email: string, contrasenia: string) {
-    const response = await axios.post('http://localhost:3000/usuarios/registro', {
+    const response = await axios.post(`${this.apiUrl}/registro`, {
       usuario: nombre_usuario,
       email,
       contrasenia,
     });
     return response.data;
   }
+
   async comprobarUsuario(usuario_ingreso: string, pass_ingreso: string) {
-    const response = await axios.post('http://localhost:3000/usuarios/ingresar', {
+    const response = await axios.post(`${this.apiUrl}/ingresar`, {
       usuario_ingreso,
       pass_ingreso,
     });
     return response.data;
   }
- 
+
   async solicitarResetContrasenia(email: string) {
-    const respuesta = await axios.post('http://localhost:3000/usuarios/olvide-contrasenia', { email });
+    const respuesta = await axios.post(`${this.apiUrl}/olvide-contrasenia`, { email });
     return respuesta.data;
   }
 
   async resetearContrasenia(token: string, nuevaContrasenia: string) {
-  const respuesta = await axios.post(`http://localhost:3000/usuarios/resetear-contrasenia/${token}`, {
-    nuevaContrasenia
-  });
-  return respuesta.data;
-}
-
-
+    const respuesta = await axios.post(`${this.apiUrl}/resetear-contrasenia/${token}`, {
+      nuevaContrasenia
+    });
+    return respuesta.data;
+  }
 }
