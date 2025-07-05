8c742130bcf725e18a8eb4b8c5afe187a2527d25
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-07-05 18:49:36 -0300

    Producto terminado

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -27,7 +27,7 @@ export class ProductoComponent implements OnInit {
     await this.obtenerUsuarioLogueado();
 }
 paginaActual = 1;
-limitePorPagina = 1;
+limitePorPagina = 3;
 totalPaginas = 1;
 usuarioLogueado: string | null = null;
 
