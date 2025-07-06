1ad2b869703b4496a889490870a82026cf4ef88b
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-07-06 12:28:44 -0300

    Carrusel

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -30,20 +30,39 @@ export class PrendasComponent implements OnInit {
     '3 cuotas sin interés con todas las tarjetas'
   ];
 
+
   imagenCarrusel: string[] = [
     'https://acdn-us.mitiendanube.com/stores/001/596/994/themes/rio/1-slide-1747670070879-2735889896-d68d7e524886b958183c4c4901f3df371747670073-1920-1920.jpg?630779295',
     'https://acdn-us.mitiendanube.com/stores/001/596/994/themes/rio/1-slide-1747669820280-978068272-cc0b4777973ad5ac5683110a8b6dc39c1747669826-1920-1920.jpg?630779295',
   ];
 
+  imagenActual: number = 0;
+
+
+
+  intervalId: any;
 
   constructor() {}
 
   async ngOnInit(): Promise<void> {
     this.obtenerUsuarioLogueado();
+    this.iniciarCarrusel();
+  }
+
+  iniciarCarrusel() {
+    this.intervalId = setInterval(() => {
+      this.imagenActual = (this.imagenActual + 1) % this.imagenCarrusel.length;
+    }, 3000);
   }
 
   obtenerUsuarioLogueado() {
     this.usuarioLogueado = localStorage.getItem('usuario');
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
    
